521b122864a8a5def231981190bf40b84b9e6015
Dhriti Prasanna Paul <52440734+dhritippaul@users.noreply.github.com>
2021-09-03 13:33:48 +0530

    Update plot_digits_classification.py

--- mnist_quiz/plot_digits_classification.py
+++ mnist_quiz/plot_digits_classification.py
@@ -2,7 +2,6 @@
 ================================
 Recognizing hand-written digits
 ================================
-
 This example shows how scikit-learn can be used to recognize images of
 hand-written digits, from 0-9.
 """
@@ -14,10 +13,13 @@ print(__doc__)
 
 # Standard scientific Python imports
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Import datasets, classifiers and performance metrics
 from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
+from skimage import data, color
+from skimage.transform import rescale, resize, downscale_local_mean
 
 ###############################################################################
 # Digits dataset
@@ -58,45 +60,134 @@ for ax, image, label in zip(axes, digits.images, digits.target):
 
 # flatten the images
 n_samples = len(digits.images)
-data = digits.images.reshape((n_samples, -1))
+print("Shape of original image : ",digits.images[0].shape)
 
-# Create a classifier: a support vector classifier
+new_image = np.zeros((n_samples,16,16))
+for i in range(0,n_samples):
+    new_image[i] = resize(digits.images[i],(16,16),anti_aliasing=True)
+data = new_image.reshape((n_samples, -1))
 clf = svm.SVC(gamma=0.001)
-
-# Split data into 50% train and 50% test subsets
 X_train, X_test, y_train, y_test = train_test_split(
     data, digits.target, test_size=0.5, shuffle=False)
-
-# Learn the digits on the train subset
-clf.fit(X_train, y_train)
-
-# Predict the value of the digit on the test subset
+clf.fit(X_train, y_train)  
 predicted = clf.predict(X_test)
+print("Accuracy with 16*16 and 50 percent both training and test data " , metrics.accuracy_score(predicted , y_test))
+
+new_image = np.zeros((n_samples,16,16))
+for i in range(0,n_samples):
+    new_image[i] = resize(digits.images[i],(16,16),anti_aliasing=True)
+data = new_image.reshape((n_samples, -1))
+clf = svm.SVC(gamma=0.001)
+X_train, X_test, y_train, y_test = train_test_split(
+    data, digits.target, test_size=0.3, shuffle=False)
+clf.fit(X_train, y_train)  
+predicted = clf.predict(X_test)
+print("Accuracy with 16*16 on 70 percent training and 30 percent test data " , metrics.accuracy_score(predicted , y_test))
+
+
+new_image = np.zeros((n_samples,16,16))
+for i in range(0,n_samples):
+    new_image[i] = resize(digits.images[i],(16,16),anti_aliasing=True)
+data = new_image.reshape((n_samples, -1))
+clf = svm.SVC(gamma=0.001)
+X_train, X_test, y_train, y_test = train_test_split(
+    data, digits.target, test_size=0.4, shuffle=False)
+clf.fit(X_train, y_train)  
+predicted = clf.predict(X_test)
+print("Accuracy with 16*16 on 60 percent training and 40 percent test data " , metrics.accuracy_score(predicted , y_test))
+
+
+new_image = np.zeros((n_samples,32,32))
+for i in range(0,n_samples):
+    new_image[i] = resize(digits.images[i],(32,32),anti_aliasing=True)
+data = new_image.reshape((n_samples, -1))
+clf = svm.SVC(gamma=0.001)
+X_train, X_test, y_train, y_test = train_test_split(
+    data, digits.target, test_size=0.5, shuffle=False)
+clf.fit(X_train, y_train)  
+predicted = clf.predict(X_test)
+print("Accuracy with 32*32 and 50 percent both training and test data " , metrics.accuracy_score(predicted , y_test))
+
+new_image = np.zeros((n_samples,32,32))
+for i in range(0,n_samples):
+    new_image[i] = resize(digits.images[i],(32,32),anti_aliasing=True)
+data = new_image.reshape((n_samples, -1))
+clf = svm.SVC(gamma=0.001)
+X_train, X_test, y_train, y_test = train_test_split(
+    data, digits.target, test_size=0.3, shuffle=False)
+clf.fit(X_train, y_train)  
+predicted = clf.predict(X_test)
+print("Accuracy with 32*32 on 70 percent training and 30 percent test data " , metrics.accuracy_score(predicted , y_test))
+
+
+
+new_image = np.zeros((n_samples,32,32))
+for i in range(0,n_samples):
+    new_image[i] = resize(digits.images[i],(32,32),anti_aliasing=True)
+data = new_image.reshape((n_samples, -1))
+clf = svm.SVC(gamma=0.001)
+X_train, X_test, y_train, y_test = train_test_split(
+    data, digits.target, test_size=0.4, shuffle=False)
+clf.fit(X_train, y_train)  
+predicted = clf.predict(X_test)
+print("Accuracy with 32,32 on 60 percent training and 40 percent test data " , metrics.accuracy_score(predicted , y_test))
+
+new_image = np.zeros((n_samples,64,64))
+for i in range(0,n_samples):
+    new_image[i] = resize(digits.images[i],(64,64),anti_aliasing=True)
+data = new_image.reshape((n_samples, -1))
+clf = svm.SVC(gamma=0.001)
+X_train, X_test, y_train, y_test = train_test_split(
+    data, digits.target, test_size=0.5, shuffle=False)
+clf.fit(X_train, y_train)  
+predicted = clf.predict(X_test)
+print("Accuracy with 64*64 and 50 percent both training and test data " , metrics.accuracy_score(predicted , y_test))
+
+new_image = np.zeros((n_samples,64,64))
+for i in range(0,n_samples):
+    new_image[i] = resize(digits.images[i],(64,64),anti_aliasing=True)
+data = new_image.reshape((n_samples, -1))
+clf = svm.SVC(gamma=0.001)
+X_train, X_test, y_train, y_test = train_test_split(
+    data, digits.target, test_size=0.4, shuffle=False)
+clf.fit(X_train, y_train)  
+predicted = clf.predict(X_test)
+print("Accuracy with 64,64on 60 percent training and 40 percent test data " , metrics.accuracy_score(predicted , y_test))
+
+new_image = np.zeros((n_samples,64,64))
+for i in range(0,n_samples):
+    new_image[i] = resize(digits.images[i],(64,64),anti_aliasing=True)
+data = new_image.reshape((n_samples, -1))
+clf = svm.SVC(gamma=0.001)
+X_train, X_test, y_train, y_test = train_test_split(
+    data, digits.target, test_size=0.3, shuffle=False)
+clf.fit(X_train, y_train)  
+predicted = clf.predict(X_test)
+print("Accuracy with 64*64 on 70 percent training and 30 percent test data " , metrics.accuracy_score(predicted , y_test))
+
+
+
 
 ###############################################################################
 # Below we visualize the first 4 test samples and show their predicted
 # digit value in the title.
 
-_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, prediction in zip(axes, X_test, predicted):
-    ax.set_axis_off()
-    image = image.reshape(8, 8)
-    ax.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
-    ax.set_title(f'Prediction: {prediction}')
+# _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
+# for ax, image, prediction in zip(axes, X_test, predicted):
+#     ax.set_axis_off()
+#     image = image.reshape(8, 8)
+#     ax.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+#     ax.set_title(f'Prediction: {prediction}')
 
-###############################################################################
-# :func:`~sklearn.metrics.classification_report` builds a text report showing
-# the main classification metrics.
+# ###############################################################################
+# # :func:`~sklearn.metrics.classification_report` builds a text report showing
+# # the main classification metrics.
 
-print(f"Classification report for classifier {clf}:\n"
-      f"{metrics.classification_report(y_test, predicted)}\n")
 
-###############################################################################
-# We can also plot a :ref:`confusion matrix <confusion_matrix>` of the
-# true digit values and the predicted digit values.
+# ###############################################################################
+# # We can also plot a :ref:`confusion matrix <confusion_matrix>` of the
+# # true digit values and the predicted digit values.
 
-disp = metrics.plot_confusion_matrix(clf, X_test, y_test)
-disp.figure_.suptitle("Confusion Matrix")
-print(f"Confusion matrix:\n{disp.confusion_matrix}")
 
-plt.show()
+
+# plt.show()
